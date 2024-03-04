12afc88399c2f585ea549d3a8a3da1b7b9d8c9c5
LuisG <lggsanchez21@gmail.com>
2024-03-04 10:26:54 +0100

    add private

--- Actividad12_Casa/src/casa/Casa.java
+++ Actividad12_Casa/src/casa/Casa.java
@@ -1,10 +1,54 @@
 package casa;
 
 public class Casa {
-	Direccion direccion;
-	double precio;
-	boolean esSegundaMano;
-	int metrosCuadrados;
-	Jardin jardin;
-	Habitacion habitacion;
+	private Direccion direccion;
+	private double precio;
+	private boolean esSegundaMano;
+	private int metrosCuadrados;
+	private Jardin jardin;
+	private Habitacion habitacion;
+	public Direccion getDireccion() {
+		return direccion;
+	}
+	public void setDireccion(Direccion direccion) {
+		this.direccion = direccion;
+	}
+	public double getPrecio() {
+		return precio;
+	}
+	public void setPrecio(double precio) {
+		this.precio = precio;
+	}
+	public boolean isEsSegundaMano() {
+		return esSegundaMano;
+	}
+	public void setEsSegundaMano(boolean esSegundaMano) {
+		this.esSegundaMano = esSegundaMano;
+	}
+	public int getMetrosCuadrados() {
+		return metrosCuadrados;
+	}
+	public void setMetrosCuadrados(int metrosCuadrados) {
+		this.metrosCuadrados = metrosCuadrados;
+	}
+	public Jardin getJardin() {
+		return jardin;
+	}
+	public void setJardin(Jardin jardin) {
+		this.jardin = jardin;
+	}
+	public Habitacion getHabitacion() {
+		return habitacion;
+	}
+	public void setHabitacion(Habitacion habitacion) {
+		this.habitacion = habitacion;
+	}
+	@Override
+	public String toString() {
+		return "Casa [precio=" + precio + ", esSegundaMano=" + esSegundaMano + ", metrosCuadrados=" + metrosCuadrados
+				+ "]";
+	}
+	
+	
+	
 }
