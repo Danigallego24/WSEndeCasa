2ba54e0cc3e0ec2f889c16b299cc22b85f583366
LuisG <lggsanchez21@gmail.com>
2024-03-04 10:21:36 +0100

    add Casa y Main_metodos

---
+++ Actividad12_Casa/src/casa/Casa.java
@@ -0,0 +1,10 @@
+package casa;
+
+public class Casa {
+	Direccion direccion;
+	double precio;
+	boolean esSegundaMano;
+	int metrosCuadrados;
+	Jardin jardin;
+	Habitacion habitacion;
+}
---
+++ Actividad12_Casa/src/casa/Main_Metodos.java
@@ -0,0 +1,9 @@
+package casa;
+
+public class Main_Metodos {
+
+	public static void main(String[] args) {
+		
+	}
+
+}
