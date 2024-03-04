7dec42e477b1766ed949e6527205eb418de11b27
dgallegoa <danigallegoarense@gmail.com>
2024-03-04 10:42:51 +0100

    add comentarios a clase Habitacion & enumHabitacion

--- Actividad12_Casa/src/casa/Habitacion.java
+++ Actividad12_Casa/src/casa/Habitacion.java
@@ -2,6 +2,8 @@ package casa;
 
 public class Habitacion {
 	
+	//A continuación se muestran los atributos de la clase Habitacion
+	
 	private String nombre;
 	private double metrosCuadrados;
 	private enumHabitacion tipoHabitacion;
--- Actividad12_Casa/src/casa/enumHabitacion.java
+++ Actividad12_Casa/src/casa/enumHabitacion.java
@@ -2,6 +2,8 @@ package casa;
 
 public enum enumHabitacion {
 	
+	//A continuación se muestran los enumerados del tipo de habitacion
+	
 	SALON, COCINA, BAÑO, SALA_DE_ESTAR
 
 }
